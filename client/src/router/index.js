bfc38b166703a142bec29f393579c73a68590211
Stephan Griesel <sgriesel@gmail.com>
2023-02-26 14:27:16 +0100

    books set up

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import MyBooks from '../components/MyBooks.vue';
 import MyPing from '../components/MyPing.vue';
 
 Vue.use(Router);
@@ -8,6 +9,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'MyBooks',
+      component: MyBooks,
+    },
     {
       path: '/',
       name: 'MyPing',
