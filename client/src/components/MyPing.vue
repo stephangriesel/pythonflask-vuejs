dd297096b500cbd05644294e350ebf7457758734
Stephan Griesel <sgriesel@gmail.com>
2023-02-26 11:56:28 +0100

    client

---
+++ client/src/components/MyPing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyPing',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+};
+</script>
